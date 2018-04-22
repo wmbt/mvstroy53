1aa4be931292beaac1e16ce06e1c52e0e888c429
wombat <ben.overlord@gmail.com>
2018-04-22 21:56:14 +0300

    Все готово

--- app/mod_constructing/controllers.py
+++ app/mod_constructing/controllers.py
@@ -9,5 +9,5 @@ mod_constructing = Blueprint('constructing', __name__, '/')
 @register_menu(mod_constructing, '.constructing', 'Строительство', order=6)
 @mod_constructing.route('/constructing', methods=['GET'])
 def index():
-    photos_list = get_photos_list(lambda x: x.startswith('ventilation'))
+    photos_list = get_photos_list(lambda x: x.startswith('constructing'))
     return render_template("constructing/index.html", photos_list=photos_list)
--- app/templates/home/readme.txt
+++
@@ -1,6 +0,0 @@
-Thanks for downloading this theme!
-
-Theme Name: Gp
-Theme URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-templat/
-Author: BootstrapMade
-Author URL: https://bootstrapmade.com
